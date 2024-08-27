d81a7f31bec9900c5753f42582195924a71d6650
Licaxne <109153143+Licaxne@users.noreply.github.com>
2024-08-27 18:41:28 +0000

    Mundo guardado 2024-08-27

--- server.py
+++ server.py
@@ -7,7 +7,8 @@
 
 # Regiones de ngrok
 # Código          Lugar
-#-----------      ---------------------------
+#-----------      ---------------------------1
+
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
 # eu		      Europa (Frankfurt)
